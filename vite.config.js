d726f752096e78d2ddbb4bdd1a679d55b6ecffbd
Abhay Dharmik <dharmikabhay362@gmail.com>
2025-06-24 06:48:56 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 5173, // or any port
+    port: 5174, // or any port
   },
 })
-
-// Ready to Explore the World of Open Source?
